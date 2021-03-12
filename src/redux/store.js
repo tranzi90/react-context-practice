2866549c9ad1ef3c877a397f604c7b07bf445dd7
denis <tranzi90@gmail.com>
2021-03-12 23:13:01 +0300

    Initial commit

---
+++ src/redux/store.js
@@ -0,0 +1,22 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import { persistStore } from 'redux-persist';
+import logger from 'redux-logger';
+
+import rootReducer from './root-reducer';
+
+const middlewares = [];
+
+if (process.env.NODE_ENV === 'development') {
+  middlewares.push(logger);
+}
+
+// export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, composeEnhancers(
+    applyMiddleware(...middlewares)
+));
+
+export const persistor = persistStore(store);
+
+export {store}
