1973e775fb7bc82ba0eac6d16be048fb82bee35c
denis <tranzi90@gmail.com>
2021-03-25 22:38:14 +0300

    payment button refactoring

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import StripeCheckout from 'react-stripe-checkout'
 
 const StripeCheckoutButton = ({ price }) => {
-    const priceForStripe = price * 100
     const publishableKey = 'pk_test_WBqax2FWVzS9QlpJScO07iuL'
 
     const onToken = (token) => {
@@ -18,7 +17,7 @@ const StripeCheckoutButton = ({ price }) => {
             shippingAddress
             image="https://svgshare.com/i/CUz.svg"
             description={`Your total is $${price}`}
-            amount={priceForStripe}
+            amount={price * 100}
             panelLabel="Pay Now"
             token={onToken}
             stripeKey={publishableKey}
