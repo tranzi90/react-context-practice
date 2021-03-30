c87ada12ce7fa7d9f06c416a02097d5e66b2d524
denis <tranzi90@gmail.com>
2021-03-30 23:07:11 +0300

    firebase.utils.js refactor

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -45,5 +45,3 @@ export const firestore = firebase.firestore()
 const provider = new firebase.auth.GoogleAuthProvider()
 provider.setCustomParameters({ prompt: 'select_account' })
 export const signInWithGoogle = () => auth.signInWithPopup(provider)
-
-export default firebase
