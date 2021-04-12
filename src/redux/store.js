755f996e8accfdeb3e2d37da89a8db5db285f6d1
denis <tranzi90@gmail.com>
2021-04-12 22:47:33 +0300

    store.js clean up

--- src/redux/store.js
+++ src/redux/store.js
@@ -10,8 +10,6 @@ if (process.env.NODE_ENV === 'development') {
     middlewares.push(logger)
 }
 
-// export const store = createStore(rootReducer, applyMiddleware(...middlewares));
-
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 const store = createStore(
     rootReducer,
