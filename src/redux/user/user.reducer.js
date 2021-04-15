187362861a0ad0b420aee63000cb7d00d8c52194
denis <tranzi90@gmail.com>
2021-04-15 23:34:23 +0300

    refactor user.reducer.js

--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -5,15 +5,12 @@ const INITIAL_STATE = {
 }
 
 const userReducer = (state = INITIAL_STATE, action) => {
-    switch (action.type) {
-        case UserActionTypes.SET_CURRENT_USER:
-            return {
-                ...state,
-                currentUser: action.payload,
-            }
-        default:
-            return state
-    }
+    if (action.type === UserActionTypes.SET_CURRENT_USER)
+        return {
+            ...state,
+            currentUser: action.payload,
+        }
+    else return state
 }
 
 export default userReducer
