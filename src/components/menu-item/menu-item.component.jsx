8ac7241a10448bd52675e95b6e312c966b2c1bca
denis <tranzi90@gmail.com>
2021-04-23 22:34:41 +0300

    refactor++

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -16,7 +16,6 @@ const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
         />
         <div className="content">
             <h1 className="title">{title.toUpperCase()}</h1>
-            <span className="subtitle">GO TO SHOP</span>
         </div>
     </div>
 )
